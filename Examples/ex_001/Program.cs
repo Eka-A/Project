11971561b70776b5edf28dc0cd698360dbb66a65
Ekaterina Anikina <reityan97@mail.ru>
2022-07-11 21:20:41 +0300

    Поиск максимального из 2х чисел

---
+++ Examples/ex_001/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine ("Введите число a: ");
+int number_a = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine ("Введите число b: ");
+int number_b = Convert.ToInt32 (Console.ReadLine());
+if (number_a > number_b)
+{
+    Console.WriteLine (number_a);
+}
+else
+{
+    Console.WriteLine (number_b);
+}
