27fbb003015d25c28360bcc6a27517da6dc08aca
Ekaterina Anikina <reityan97@mail.ru>
2022-07-12 21:56:17 +0300

    Вывод всех чётных чисел

---
+++ Examples/ex_004/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine ("Введите число: ");
+int number = Convert.ToInt32 (Console.ReadLine());
+int i = 1;
+
+ while (i <= number)
+            {
+                int result = i % 2;
+                if (result != 1)
+                {
+                    Console.WriteLine("четное " + i);
+                    i++;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+        
